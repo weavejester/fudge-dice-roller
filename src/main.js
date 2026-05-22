feeac22e314f2097b24b056cc0eab93a5505f65e
James Reeves <jreeves@weavejester.com>
2026-05-23 00:23:40 +0100

    Integrate roll button with Dice+

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import OBR from "@owlbear-rodeo/sdk";
 
-function rollFudgeDice() {
-  OBR.notification.show("Rolling dice");
+async function rollFudgeDice() {
+  await OBR.broadcast.sendMessage("dice-plus/roll-request", {
+    rollId: `roll_${Date.now()}_${Math.random().toString(36).substring(2, 9)}`,
+    playerId: await OBR.player.getId(),
+    playerName: await OBR.player.getName(),
+    rollTarget: 'everyone',
+    diceNotation: '4dF',
+    showResults: true,
+    timestamp: Date.now(),
+    source: "fateroll",
+  }, { destination: 'ALL' });
 }
 
 document.querySelector('#rollfudge').addEventListener('click', rollFudgeDice);
